bb125c44600fc7784f7104c8350b8694b6a7c96a
ROSTAND LELE <rostoinfocus@gmail.com>
2025-01-19 08:30:19 +0100

    autrs

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.vue',
+        // './resources/js/**/*.js',
+        // './resources/js/**/*.ts',
     ],
 
     theme: {
